9b789080622be543da3c4950084d567c1b8d5c36
rhhdez29 <rafaelhdez290@gmail.com>
2025-11-12 20:04:12 -0600

    cambios

---
+++ src/app/screens/maestros-screen/maestros-screen.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { FacadeService } from 'src/app/services/facade.service';
+import { MaestrosService } from 'src/app/services/maestros.service';
+import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+
+@Component({
+  selector: 'app-maestros-screen',
+  templateUrl: './maestros-screen.component.html',
+  styleUrls: ['./maestros-screen.component.scss']
+})
+
+export class MaestrosScreenComponent implements OnInit {
+
+  public name_user: string = "";
+  public rol: string = "";
+  public token: string = "";
+  public lista_maestros: any[] = [];
+
+  //Para la tabla
+  displayedColumns: string[] = ['id_trabajador', 'nombre', 'email', 'fecha_nacimiento', 'telefono', 'rfc', 'cubiculo', 'area_investigacion', 'materias_json', 'editar', 'eliminar'];
+  dataSource = new MatTableDataSource<DatosUsuario>(this.lista_maestros as DatosUsuario[]);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  constructor(
+    public facadeService: FacadeService,
+    public maestrosService: MaestrosService,
+    private router: Router,
+    private _liveAnnouncer: LiveAnnouncer
+  ) { }
+
+  ngOnInit(): void {
+    this.name_user = this.facadeService.getUserCompleteName();
+    this.rol = this.facadeService.getUserGroup();
+    //Validar que haya inicio de sesión
+    //Obtengo el token del login
+    this.token = this.facadeService.getSessionToken();
+    console.log("Token: ", this.token);
+    if(this.token == ""){
+      this.router.navigate(["/"]);
+    }
+    //Obtener maestros
+    this.obtenerMaestros();
+  }
+
+  // Consumimos el servicio para obtener los maestros
+  //Obtener maestros
+  public obtenerMaestros() {
+    this.maestrosService.obtenerListaMaestros().subscribe(
+      (response) => {
+        this.lista_maestros = response;
+        console.log("Lista users: ", this.lista_maestros);
+        if (this.lista_maestros.length > 0) {
+          //Agregar datos del nombre e email
+          this.lista_maestros.forEach(usuario => {
+            usuario.first_name = usuario.user.first_name;
+            usuario.last_name = usuario.user.last_name;
+            usuario.email = usuario.user.email;
+          });
+          console.log("Maestros: ", this.lista_maestros);
+
+          this.dataSource = new MatTableDataSource<DatosUsuario>(this.lista_maestros as DatosUsuario[]);
+        }
+      }, (error) => {
+        console.error("Error al obtener la lista de maestros: ", error);
+        alert("No se pudo obtener la lista de maestros");
+      }
+    );
+  }
+
+  public goEditar(idUser: number) {
+    this.router.navigate(["registro-usuarios/maestros/" + idUser]);
+  }
+
+  public delete(idUser: number) {
+
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    console.log("SortState: ", sortState.direction);
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+
+  }
+
+}
+
+//Esto va fuera de la llave que cierra la clase
+export interface DatosUsuario {
+  id: number,
+  id_trabajador: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  fecha_nacimiento: string,
+  telefono: string,
+  rfc: string,
+  cubiculo: string,
+  area_investigacion: number,
+  materias_json: string[];
+}
